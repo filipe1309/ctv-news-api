531d435e488db5715d8b6e9cc366c2f48fe55441
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-04-02 20:19:15 -0300

    refactor: deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,8 @@
 echo "#############################################"
 echo "                   DEPLOY                   "
 echo "#############################################"
-echo "# Optional param: --tag-msg TAG_MESSAGE_HERE"
+echo "# [Optional] param: --tag-msg \"TAG_MESSAGE_HERE\""
+echo "---------------------------------------------"
 
 TAG_MSG=$2
 GIT_BRANCH=$(git branch --show-current)
@@ -16,8 +17,9 @@ echo "Deploying branch: $GIT_BRANCH ..."
 if [ $# -eq 0 ]; then
     echo "Type the tag message:"
     echo "# example: \"$(git tag -n9 | head -n 1 | awk '{for(i=2;i<=NF;++i)printf $i FS}')\""
-    read tagmsg
-    if [ $tagmsg ]; then
+    read -e tagmsg
+    echo "Typed: \"$tagmsg\""
+    if [ ! -z "$tagmsg"  -a "$tagmsg" != " " ]; then
         TAG_MSG=$tagmsg
     else
         echo "Tag message missing"
@@ -31,16 +33,17 @@ else
     fi
 fi
 
+echo "---------------------------------------------"
 echo "Branch to deploy will be: \"$GIT_BRANCH\""
-echo "Tag name will be: \"$TAG_NAME\""
-echo "Tag message will be: \"$TAG_MSG\""
-
+echo "Tag will be: [name]= \"$TAG_NAME\", [msg]= \"$TAG_MSG\""
+echo "---------------------------------------------"
 
 read -r -p "Are you sure? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
+    echo "---------------------------------------------"
     echo "Deploying..."
-    #git tag -a $TAG_NAME -m $TAG_MSG && git ps origin $GIT_BRANCH && git ps origin $GIT_BRANCH --tags && git co main && git pl
+    git tag -a $TAG_NAME -m $TAG_MSG && git ps origin $GIT_BRANCH && git ps origin $GIT_BRANCH --tags && git co main && git pl
     echo "Deploy completed!"
 else
     echo "Bye =)"
