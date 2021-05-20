0cdad406a08ca4e4c0af5ba49bc35c3abde72950
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-20 09:21:46 -0300

    chore: add validation middleware to rotutes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+use App\Models\Author\Author;
+use App\Models\ImageNews\ImageNews;
+use App\Models\News\News;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -17,22 +21,34 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\Author'], function () use ($router) {
-    $router->post('/authors', 'AuthorController@create');
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\Author', 'as', Author::class], function () use ($router) {
+    $router->post('/authors', [
+        'uses' => 'AuthorController@create',
+        'middleware' => 'ValidateDataMiddleware'
+    ]);
 
     $router->get('/authors', 'AuthorController@findAll');
 
     $router->get('/authors/{id}', 'AuthorController@findOneBy');
 
-    $router->put('/authors/{param}', 'AuthorController@editBy');
+    $router->put('/authors/{param}', [
+        'uses' => 'AuthorController@editBy',
+        'middleware' => 'ValidateDataMiddleware'
+    ]);
 
-    $router->patch('/authors/{param}', 'AuthorController@editBy');
+    $router->patch('/authors/{param}', [
+        'uses' => 'AuthorController@editBy',
+        'middleware' => 'ValidateDataMiddleware'
+    ]);
 
     $router->delete('/authors/{id}', 'AuthorController@delete');
 });
 
-$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\News'], function () use ($router) {
-    $router->post('/news', 'NewsController@create');
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\News', 'as' => News::class], function () use ($router) {
+    $router->post('/news', [
+        'uses' => 'NewsController@create',
+        'middleware' => 'ValidateDataMiddleware'
+    ]);
 
     $router->get('/news', 'NewsController@findAll');
 
@@ -40,17 +56,26 @@ $router->group(['prefix' => 'api/v1', 'namespace' => 'V1\News'], function () use
 
     $router->get('/news/{param}', 'NewsController@findBy');
 
-    $router->put('/news/{param}', 'NewsController@editBy');
+    $router->put('/news/{param}', [
+        'uses' => 'NewsController@editBy',
+        'middleware' => 'ValidateDataMiddleware'
+    ]);
 
-    $router->patch('/news/{param}', 'NewsController@editBy');
+    $router->patch('/news/{param}', [
+        'uses' => 'NewsController@editBy',
+        'middleware' => 'ValidateDataMiddleware'
+    ]);
 
     $router->delete('/news/{param}', 'NewsController@deleteBy');
 
     $router->delete('/news/author/{authorId}', 'NewsController@deleteByAuthor');
 });
 
-$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\ImageNews'], function () use ($router) {
-    $router->post('/image-news', 'ImageNewsController@create');
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\ImageNews', 'as' => ImageNews::class], function () use ($router) {
+    $router->post('/image-news', [
+        'uses' => 'ImageNewsController@create',
+        'middleware' => 'ValidateDataMiddleware'
+    ]);
 
     $router->get('/image-news', 'ImageNewsController@findAll');
 
@@ -58,9 +83,15 @@ $router->group(['prefix' => 'api/v1', 'namespace' => 'V1\ImageNews'], function (
 
     $router->get('/image-news/{id}', 'ImageNewsController@findOneBy');
 
-    $router->put('/image-news/{param}', 'ImageNewsController@editBy');
+    $router->put('/image-news/{param}', [
+        'uses' => 'ImageNewsController@editBy',
+        'middleware' => 'ValidateDataMiddleware'
+    ]);
 
-    $router->patch('/image-news/{param}', 'ImageNewsController@editBy');
+    $router->patch('/image-news/{param}', [
+        'uses' => 'ImageNewsController@editBy',
+        'middleware' => 'ValidateDataMiddleware'
+    ]);
 
     $router->delete('/image-news/{id}', 'ImageNewsController@delete');
 
